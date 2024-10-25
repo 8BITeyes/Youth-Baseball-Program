8ad25be9513a514d9eb7fa4b5c251fdbf64dbd96
RyanF <RyanF@DESKTOP-5RA985F>
2024-10-24 21:55:46 -0700

    Add project files.

---
+++ CS232 Youth Baseball - Exam 4/Source.cpp
@@ -0,0 +1,33 @@
+/* 1. The line-up should include batting order and player’s defensive positions for each of the five innings.
+
+2. Each team shall play all players in the field in defensive positions. Additional players, (short fielders), shall be
+positioned in the outfield no closer than 20 feet to the baseline when the ball is hit and will be considered
+outfielders.
+
+3. Each player must be scheduled to play an infield position at least one (1) inning. Catcher will be considered an
+infield position. A player may only be scheduled to play catcher a maximum of one (1) inning per game. No
+player may play a second inning of infield until every other player has played one inning of infield.
+
+4. A player cannot play the same defensive position for more than one (1) inning per game.
+
+5. A player can only sit out one (1) inning per game.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	//declarations
+	const int NUMOFPLAYERS = 12;
+	const int NUMOFINNINGS = 5;
+	const int NUMOFINFIELDPOSITIONS = 6;
+	const int NUMOFOUTFIELDPOSITIONS = 4;
+	
+	
+	int averages[NUMOFPLAYERS];
+
+
+
+
+	return 0;
+}
