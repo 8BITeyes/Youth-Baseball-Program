94ebd3442068a329555d6f47e3ead964849c7003
Ryan Frazin <RyanFrazin@Gmail.com>
2024-11-06 22:51:38 -0800

    Added the sortLinup function

--- CS232 Youth Baseball - Exam 4/Source.cpp
+++ CS232 Youth Baseball - Exam 4/Source.cpp
@@ -15,56 +15,95 @@ player may play a second inning of infield until every other player has played o
 
 #include <iostream>
 #include <string>
+#include <cstdio>
+#include <random>
 using namespace std;
 
-const int NUMOFPLAYERS = 12;
+struct player {
+	string name;
+	int average{};
+};
 
-int* sortPlayerAverages(int playerAverageArray[], int sortedPlayerAverages[]);
+//Constant declarations
+const int NUMOFPLAYERS = 12;
+const int NUMOFINNINGS = 5;
+const int MIN = 0;
+const string POSITIONS[NUMOFPLAYERS] = { "P", "C", "1B", "2B", "SS", "3B", "RF", "LF", "RCF", "LCF", "OUT" };
+
+//Function declarations
+player* sortPlayers(player playersArray[], player sortedPlayerAverages[]);
+string displayArray(string playersArray);
+string* sortLineup(player sortedPlayerArray[], string playerLineup[][NUMOFINNINGS]);
 
 int main() {
 	//declarations
-	string playerNames[NUMOFPLAYERS];
-	int playerAverages[NUMOFPLAYERS];
-	int sortedPlayerAveragesArray[NUMOFPLAYERS];
+	player players[NUMOFPLAYERS];
+	player sortedPlayers[NUMOFPLAYERS];
+	string playerLineup[NUMOFPLAYERS][NUMOFINNINGS + 1];
 
 	//get user input
 	cout << "Enter 12 player names: " << endl;
 	for (int i = 0; i < NUMOFPLAYERS; i++) {
 		cout << "What is player " << i + 1 << "'s name: ";
-		cin >> playerNames[i];
+		cin >> players[i].name;
 	} // Inputs the players' names into playerNames Array
 
 	cout << "\nEnter the averages for each player:" << endl << "-----------------------------------" << endl;
 	for (int i = 0; i < NUMOFPLAYERS; i++) {
-		cout << "What is " << playerNames[i] << "'s average: ";
-		cin >> playerAverages[i];
+		cout << "What is " << players[i].name << "'s average: ";
+		cin >> players[i].average;
 	} // Inputs the players' averages into an array playerAverages
 
-	sortPlayerAverages(playerAverages, sortedPlayerAveragesArray);
 
-	for (int i = 0; i < NUMOFPLAYERS; i++) {
-		cout << playerAverages[i] << endl;
-	}
+	sortPlayers(players, sortedPlayers);
+
+
+
+	//displayArray(playerLineup);
 
 	return 0;
 }
 
-int* sortPlayerAverages(int playerAverageArray[], int sortedPlayerAverages[]) {
+player* sortPlayers(player playerAverageArray[], player sortedPlayers[]) {
 	int currentMaxAverageInIteration;
+	string currentNameInIteration;
 
 	for (int i = 0; i < NUMOFPLAYERS; i++) {
-		currentMaxAverageInIteration = playerAverageArray[i];
+		currentMaxAverageInIteration = playerAverageArray[i].average;
+		currentNameInIteration = playerAverageArray[i].name;
 
 		for (int j = 1; j < NUMOFPLAYERS; j++) {
-			if (currentMaxAverageInIteration < playerAverageArray[j]) {
-				currentMaxAverageInIteration = playerAverageArray[j];
-				sortedPlayerAverages[i] = playerAverageArray[j];
-				playerAverageArray[j] = currentMaxAverageInIteration;
+			if (currentMaxAverageInIteration < playerAverageArray[j].average) {
+				currentMaxAverageInIteration = playerAverageArray[j].average;
+				sortedPlayers[i].average = playerAverageArray[j].average;
+				sortedPlayers[i].name = playerAverageArray[j].name;
+				playerAverageArray[j].average = currentMaxAverageInIteration;
 			}
 		}
 
-		sortedPlayerAverages[i] = currentMaxAverageInIteration;
+		sortedPlayers[i].average = currentMaxAverageInIteration;
+		sortedPlayers[i].name = currentNameInIteration;
 	}
 
-	return sortedPlayerAverages;
+	return sortedPlayers;
+}
+
+string* sortLineup(player sortedPlayerArray[], string playerLineup[][NUMOFINNINGS]) {
+	srand((unsigned) time(NULL));
+
+	for (int i = MIN; i < NUMOFPLAYERS; i++) {
+		playerLineup[i][0] = sortedPlayerArray[i].name;
+	}// Inputs the players names in order into the first column of the lineup (2D array)
+
+
+	for (int i = MIN + 1; i < NUMOFPLAYERS; i++) {
+		int random = rand() % 12; //random number that will be generated
+	} //populates the rows after the name row
+};
+
+string displayArray(string playersArray) {
+	cout << "Game lineup and field positions:" << endl;
+	cout << "________________________________" << endl;
+	cout << "Name \t Inning 1 \t Inning 2 \t Inning 3 \t Inning 4 \t Inning 5" << endl;
+
 }
